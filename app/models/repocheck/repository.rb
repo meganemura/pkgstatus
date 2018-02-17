1a4f3e29c36cf97743157272ced3df8fc044f62c
meganemura <meganemura@users.noreply.github.com>
2018-02-18 02:05:49 +0900

    Fix indentation

--- app/models/repocheck/repository.rb
+++ app/models/repocheck/repository.rb
@@ -1,22 +1,22 @@
 module Repocheck
-class Repository
-  # FIXME: Detect repository service from uri
-  def self.from_url(uri)
-    Repositories::GithubRepository.new(uri)
-  end
+  class Repository
+    # FIXME: Detect repository service from uri
+    def self.from_url(uri)
+      Repositories::GithubRepository.new(uri)
+    end
 
-  def initialize(uri)
-    @url = uri
-  end
+    def initialize(uri)
+      @url = uri
+    end
 
-  attr_reader :uri
+    attr_reader :uri
 
-  def self.metric_classes
-    []
-  end
+    def self.metric_classes
+      []
+    end
 
-  def metrics
-    self.class.metric_classes.map { |klass| klass.new(self) }
+    def metrics
+      self.class.metric_classes.map { |klass| klass.new(self) }
+    end
   end
-end
 end
