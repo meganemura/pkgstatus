fba3b38fb23c9bc74a3241aa8a55d5c8b288cfd4
meganemura <meganemura@users.noreply.github.com>
2018-02-18 03:03:22 +0900

    Implement projects#show

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,13 @@
 class Project
+  def self.find(id)
+    new
+  end
+
+  # FIXME: Dummy
+  def packages
+    [
+      %w(rubygems codestatus),
+      %w(npm react),
+    ].map { |registry, name| Package.find_by(registry: registry, name: name) }
+  end
 end
