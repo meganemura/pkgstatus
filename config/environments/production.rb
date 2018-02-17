36e6ae90772b3d467784670d95a1b0f6f48ec1c2
meganemura <meganemura@users.noreply.github.com>
2018-02-17 23:07:23 +0900

    Use redis as a cache store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,8 +51,8 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  # Use redis as a cache store
+  config.cache_store = :redis_store, File.join(ENV['REDIS_URL'].presence, '0', 'cache'), { expires_in: 90.minutes }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
