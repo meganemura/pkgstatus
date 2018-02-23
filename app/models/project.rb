4f85a2dff881d3bafd4bb379d8acaa05cea32873
meganemura <meganemura@users.noreply.github.com>
2018-02-24 01:24:40 +0900

    Sample packages in development

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,10 +4,26 @@ class Project
   end
 
   def packages
-    pkgs = []
-    pkgs += File.read(Rails.root.join('db', 'static', 'mastodon-rubygems.list')).lines.map { |x| ['rubygems', x.chomp] }
-    pkgs += File.read(Rails.root.join('db', 'static', 'mastodon-npm.list')).lines.map { |x| ['npm', x.chomp] }
+    x = sample_rubygems_packages.map { |x| ['rubygems', x] }
+    y = sample_npm_packages.map { |x| ['npm', x] }
+
+    if Rails.env.development?
+      x = x.sample(10)
+      y = y.sample(10)
+    end
+
+    pkgs = x + y
 
     pkgs.map { |registry, name| Package.find_by(registry: registry, name: name) }
+  end
+
+  private
+
+  def sample_rubygems_packages
+    File.read(Rails.root.join('db', 'static', 'mastodon-rubygems.list')).lines.map(&:chomp)
+  end
+
+  def sample_npm_packages
+    File.read(Rails.root.join('db', 'static', 'mastodon-npm.list')).lines.map(&:chomp)
   end
 end
