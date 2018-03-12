8445a9011e4dac7e1d62441f91b6ce61791c446b
meganemura <meganemura@users.noreply.github.com>
2018-03-13 01:07:22 +0900

    Implement LatestReleaseMetric

---
+++ app/models/packary/metrics/registry_package/latest_release_metric.rb
@@ -0,0 +1,16 @@
+module Packary
+  class Metrics::RegistryPackage::LatestReleaseMetric < Packary::Metric
+    def self.title
+      'Latest Releases'
+    end
+
+    def read(source)
+      Time.parse(source.gem_versions.first['built_at'])
+    end
+
+    def status
+      return :success if value > 180.days.ago
+      :warning
+    end
+  end
+end
