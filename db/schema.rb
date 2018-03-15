0b46f057f56e2c40ac28d148636de6fdc3dd59bc
meganemura <meganemura@users.noreply.github.com>
2018-03-16 00:33:34 +0900

    $ bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_03_15_153147) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
 end
