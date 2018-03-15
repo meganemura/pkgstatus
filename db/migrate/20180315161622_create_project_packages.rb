a8660a6f0c4dcf1cf4dbe7ebe99e2337b7fa9cf0
meganemura <meganemura@users.noreply.github.com>
2018-03-16 01:17:06 +0900

    Specify type of ids

--- db/migrate/20180315161622_create_project_packages.rb
+++ db/migrate/20180315161622_create_project_packages.rb
@@ -1,8 +1,8 @@
 class CreateProjectPackages < ActiveRecord::Migration[5.2]
   def change
     create_table :project_packages, id: :uuid do |t|
-      t.belongs_to :project, foreign_key: true
-      t.belongs_to :package, foreign_key: true
+      t.belongs_to :project, foreign_key: true, type: :uuid
+      t.belongs_to :package, foreign_key: true, type: :uuid
 
       t.timestamps
     end
