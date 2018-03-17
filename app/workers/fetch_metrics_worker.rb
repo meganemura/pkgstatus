35a094724bc23e187deff29a6782813c82edb4f8
meganemura <meganemura@users.noreply.github.com>
2018-03-18 05:59:28 +0900

    Detect rate limit

--- app/workers/fetch_metrics_worker.rb
+++ app/workers/fetch_metrics_worker.rb
@@ -1,6 +1,23 @@
 class FetchMetricsWorker < ApplicationWorker
   def perform(registry, name)
+    raise 'Request rate limit exceeded' if rate_limited?
+
     package = Package.find_by(registry: registry, name: name)
     package.cache
+  rescue Octokit::TooManyRequests => e
+    self.rate_limit_reset = e.response_headers['x-ratelimit-reset'].to_i
+  end
+
+  def rate_limit_reset=(reset_time)
+    reset_in = [reset_time - Time.now.to_i, 300].max
+    Rails.cache.write(cache_key, true, expires_in: reset_in)
+  end
+
+  def rate_limited?
+    Rails.cache.exist?(cache_key)
+  end
+
+  def cache_key
+    [self.class.name, 'rate_limit_reset'].join(':')
   end
 end
