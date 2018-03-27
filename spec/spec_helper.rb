925339a4eadf3af35ffb5e6196b836d62c62ef61
meganemura <meganemura@users.noreply.github.com>
2018-03-28 00:53:41 +0900

    Configure SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,12 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+if ENV['CI'] == 'true'
+  require "simplecov"
+  SimpleCov.start
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
