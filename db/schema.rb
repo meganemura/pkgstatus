503d5e1204f2a5fb38862aea7f79477cb4c5dd7e
meganemura <meganemura@users.noreply.github.com>
2018-04-19 00:43:39 +0900

    $ bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_18_143357) do
+ActiveRecord::Schema.define(version: 2018_04_18_154130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2018_04_18_143357) do
     t.uuid "package_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "metric_type"
+    t.string "value"
     t.index ["package_id"], name: "index_metrics_on_package_id"
   end
 
