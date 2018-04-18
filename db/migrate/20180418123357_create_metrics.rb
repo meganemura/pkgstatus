fff3ec14590683937baac577c8d7e3c924ccbd63
meganemura <meganemura@users.noreply.github.com>
2018-04-18 21:34:34 +0900

    Specify type of reference column

--- db/migrate/20180418123357_create_metrics.rb
+++ db/migrate/20180418123357_create_metrics.rb
@@ -1,7 +1,7 @@
 class CreateMetrics < ActiveRecord::Migration[5.2]
   def change
     create_table :metrics, id: :uuid do |t|
-      t.belongs_to :package, foreign_key: true
+      t.belongs_to :package, foreign_key: true, type: :uuid
 
       t.timestamps
     end
