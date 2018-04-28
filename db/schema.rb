abf88c4c2460efd5207a5a4a26defbe631733c22
meganemura <meganemura@users.noreply.github.com>
2018-04-28 23:42:18 +0900

    $ bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_18_154130) do
+ActiveRecord::Schema.define(version: 2018_04_28_144053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_04_18_154130) do
     t.string "ci_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "expired_at"
     t.index ["package_id"], name: "index_package_sources_on_package_id"
   end
 
